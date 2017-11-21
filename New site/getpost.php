ddcf88525a047a8cef267b336758d6cdff8ebb3c
Tanasart <22589759+Ttanasart-pt@users.noreply.github.com>
2017-11-21 12:53:19 +0700

    123

--- New site/getpost.php
+++ New site/getpost.php
@@ -1,10 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
+<?php
+	$con = mysqli_connect("localhost", "id1718216_user", "Bloater123", "id1718216_data");
+	$post = mysqli_prepare($con, "SELECT title FROM Makham posts");
+	mysqli_stmt_execute($post);
+	mysqli_stmt_store_result()
 
-</body>
-<?php echo "<pre>echoed value</pre>" ?> 
-</html>
+?>
---
+++ New site/post-edit.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Makham Farmer</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+		body{
+			background-image: url("sorce-img/tamarind 14.png");
+			background-attachment: fixed;
+			background-repeat: no-repeat;
+		}
+		::-webkit-scrollbar {
+		    width: 2px;
+		}
+		::-webkit-scrollbar-thumb {
+		    background: rgba(51,51,51,1); 
+		}
+	</style>
+	<link rel="stylesheet" href="getposts.php">
+
+</head>
+
+<body>
+	<div style="height: 20px"></div>
+	<div align="center">
+		<div class="post-box-wide" style="height: 100%">
+			
+		</div>
+	</div>
+
+
+</body>
+
+</html>
